a9d2f7c68ac2c32cae58eada473267de8cc5c59f
Muhammed Salih Km <salihkm000@gmail.com>
2025-03-17 19:17:24 +0530

    updated

--- models/admissionModel.js
+++ models/admissionModel.js
@@ -7,14 +7,15 @@ const admissionSchema = new mongoose.Schema({
   gender: { type: String, required: true },
   houseAddress: { type: String, required: true },
   phone: { type: String, required: true },
-  identificationMarks: { type: String, required: true },
+  identificationMarks: { type: String},
   fatherName: { type: String, required: true },
   guardianName: { type: String, required: true },
   relation: { type: String, required: true },
   guardianOccupation: { type: String, required: true },
   classAdmitted: { type: String, required: true },
   formerMadrasa: { type: String },
-  certificateNumber: { type: String }
+  certificateNumber: { type: String },
+  tcNumber : { type: String },
 }, { timestamps: true });
 
 const Admission = mongoose.model("Admission", admissionSchema);
